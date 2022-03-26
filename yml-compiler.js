b9d47950e3733273c70f244085755dfcb7c3ae22
SnaveSutit <snavesutit@gmail.com>
2022-03-26 10:24:05 -0400

    Initial setup and basic schemas

---
+++ yml-compiler.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const pathjs = require('path')
+const yaml = require('js-yaml')
+const chokidar = require('chokidar')
+
+function movePath(from, to, path) {
+	return pathjs.resolve(to, pathjs.relative(from, path))
+}
+
+function compile() {
+	const files = []
+
+	function recurse(path) {
+		const filenames = fs.readdirSync(path)
+
+		for (const name of filenames) {
+			const localPath = pathjs.join(path, name)
+			const stat = fs.lstatSync(localPath)
+			if (stat.isFile()) {
+				files.push(localPath)
+				continue
+			}
+			recurse(localPath)
+		}
+	}
+	recurse('./src/schemas/')
+	console.log('Building Schemas...')
+	fs.rmSync('./schemas/', { recursive: true, force: true })
+
+	for (const file of files) {
+		const obj = yaml.load(fs.readFileSync(file, 'utf-8'))
+
+		const outFile = movePath('./src/schemas/', './schemas/', file).replace('.yml', '.json')
+		const outPath = pathjs.parse(outFile)
+		console.log(`${file} -> ${outFile}`)
+		try {
+			fs.mkdirSync(outPath.dir, { recursive: true })
+		} catch (err) {}
+		fs.writeFileSync(outFile, JSON.stringify(obj, null, '\t') || '{}')
+	}
+
+	console.log('Done!')
+}
+
+function main() {
+	const watcher = chokidar.watch('./src/schemas/').on('change', () => {
+		compile()
+	})
+}
+
+main()
